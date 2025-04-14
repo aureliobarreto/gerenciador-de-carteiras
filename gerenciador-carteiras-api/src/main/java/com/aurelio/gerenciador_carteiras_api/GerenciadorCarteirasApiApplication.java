b8c92079a6b8a108573f6793514d6681435aeb31
aurelio <aurelionadjabarreto@gmail.com>
2025-04-14 00:59:07 -0300

    feat: auth module

---
+++ gerenciador-carteiras-api/src/main/java/com/aurelio/gerenciador_carteiras_api/GerenciadorCarteirasApiApplication.java
@@ -0,0 +1,13 @@
+package com.aurelio.gerenciador_carteiras_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCarteirasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCarteirasApiApplication.class, args);
+	}
+
+}
---
+++ gerenciador-carteiras-api/src/test/java/com/aurelio/gerenciador_carteiras_api/GerenciadorCarteirasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurelio.gerenciador_carteiras_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCarteirasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
